2f3c1fe810c9f900a2c5996c3c87059abcc53849
shafiq <shafiqabs@gmail.com>
2014-01-09 12:45:55 +0600

    Initial Commit

---
+++ web/index.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('APPLICATION_ENV')
+|| define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'prod'));
+
+define('WEB_PATH', __DIR__);
+
+if (APPLICATION_ENV == 'dev') {
+    require 'app_dev.php';
+} else {
+    require 'app.php';
+}
